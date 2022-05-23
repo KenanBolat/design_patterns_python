059cb530a4eb29f76719a5964f5de8c6bb9f8555
Kenan Bolat <kenan23@gmail.com>
2022-05-23 11:27:50 +0300

    add exercise file

--- factory/2_factory_method.py
+++ factory/2_factory_method.py
@@ -15,12 +15,11 @@ class Point:
     def __str__(self):
         return f'x : {self.x} \t y:{self.y} '
 
-
     class PointFactory:
         def new_cartesian_point(self, x, y):
             return Point(x, y)
 
-         def new_polar_point(self,  rho, theta):
+        def new_polar_point(self, rho, theta):
             return Point(rho * cos(theta), rho * (sin(theta)))
 
         # def __init__(self, a, b, system=CoordinateSystem.CARTESIAN):
@@ -31,10 +30,10 @@ class Point:
         #         self.x = a*cos(b)
         #         self.y = a*sin(b)
 
-
     # like singleton
 
     factory = PointFactory()
+
 
 if __name__ == '__main__':
     p = Point(2, 3)
